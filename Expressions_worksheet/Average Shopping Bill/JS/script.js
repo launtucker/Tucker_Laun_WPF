3fe2607513c756100013a6ce0a17dcb22002170a
launtucker <launtucker@launs-MacBook-Pro.local>
2014-06-12 19:15:02 -0400

    Dog yrs js
    Finished dog yrs

--- Expressions_worksheet/Average Shopping Bill/JS/script.js
+++ Expressions_worksheet/Average Shopping Bill/JS/script.js
@@ -1,3 +1,4 @@
 /**
  * Created by launtucker on 6/12/14.
  */
+
--- Expressions_worksheet/Dog Years/JS/script.js
+++ Expressions_worksheet/Dog Years/JS/script.js
@@ -1,3 +1,8 @@
 /**
  * Created by launtucker on 6/12/14.
  */
+
+
+var humanAge = 4 //Sparky in Human years
+var dogYears = 7 * humanAge; // sparkys human years times dog years
+console.log(dogYears);//Sparkys age in dog years
