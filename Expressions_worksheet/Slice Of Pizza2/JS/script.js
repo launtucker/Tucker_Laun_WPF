32fabe9e58c60dfcdd1d522b73104b1875fabd6b
launtucker <launtucker@launs-MacBook-Pro.local>
2014-06-12 19:47:11 -0400

    Pizza slice 2
    Finished js on this assignment

--- Expressions_worksheet/Slice Of Pizza2/JS/script.js
+++ Expressions_worksheet/Slice Of Pizza2/JS/script.js
@@ -1,3 +1,16 @@
 /**
- * Created by launtucker on 6/12/14.
+ * Created by laun tucker on 6/12/14 slices of pizza 2.
+ *
+ * var slices = 10; //Slices of pizza
+ var people = 6; // Number of people at the party
+ var pizza = 4; // how many pizza's ordered
+ var totalSlices = slices * pizza; // slices in total
+ var personGets = totalSlices / people;  // people;number of slices per person
+ console.log(personGets);
  */
+
+var totalSlices = 40; // number of slices total
+var amountAte = 36; //amount ate by people
+var sparkyEats = 40 - 36;
+console.log(sparkyEats)
+
