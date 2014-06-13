de546c63a76175a028bc7f2a4a96fdeed556fa77
launtucker <launtucker@launs-MacBook-Pro.local>
2014-06-12 20:57:28 -0400

    Personal finished.
    added js on miles drove weekly

---
+++ Expressions_Personal/JS/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created by laun tucker on 6/12/14 Expressions Personal.
+ */
+
+var dailyMiles = prompt("How many miles do you drive to work?");
+var daysWorked = prompt("How many days do you work a week?");
+var weeklyTotal = dailyMiles * daysWorked;
+alert("You drive " +weeklyTotal+ " miles in a week! ")
