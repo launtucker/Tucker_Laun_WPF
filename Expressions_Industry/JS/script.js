8d88b3b02ddbbc67aa4c3c84ed3281f0085f1615
launtucker <launtucker@launs-MacBook-Pro.local>
2014-06-12 21:15:43 -0400

    Expression Industry
    added js file

---
+++ Expressions_Industry/JS/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by laun tucker on 6/12/14 Expression Industry.
+ */
+
+var timelineHours = 4;
+var designHours = 36;
+var frameworkHours = 3;
+var totalHours = timelineHours + designHours + frameworkHours;
+var averageHours = totalHours / 7;
+console.log("I designed a website in the total of " +totalHours+ " in 7 days, \n averaging out to be " +averageHours+ " daily.");
