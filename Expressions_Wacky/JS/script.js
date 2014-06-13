3a841a61c9cf47a4f4a9aa8eeb7370a9e4883acf
launtucker <launtucker@launs-MacBook-Pro.local>
2014-06-12 21:40:58 -0400

    wacky
    added wacky expression

---
+++ Expressions_Wacky/JS/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by laun tucker on 6/12/14 Expression Wacky.
+ */
+
+alert("Lets play a game!");//wacky start
+var pillows = prompt("How many pillows do you sleep with?");// total Pillows
+var square = prompt("How Many are square?"); //number of square
+var fluff = prompt("Do you have to fluff you pillows?"); //just a random question
+alert ("It doesn't really matter if you fluff your" +pillows+ " pillows you sleep with to many pillows! The " +square+ " square pillows I'm taking!")
