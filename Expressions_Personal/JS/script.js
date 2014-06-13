33eefec36f5e7007dfd687b1066162173c8cd56c
launtucker <launtucker@launs-MacBook-Pro.local>
2014-06-12 21:20:00 -0400

    Add notes
    add notes

--- Expressions_Personal/JS/script.js
+++ Expressions_Personal/JS/script.js
@@ -2,7 +2,7 @@
  * Created by laun tucker on 6/12/14 Expressions Personal.
  */
 
-var dailyMiles = prompt("How many miles do you drive to work?");
-var daysWorked = prompt("How many days do you work a week?");
-var weeklyTotal = dailyMiles * daysWorked;
+var dailyMiles = prompt("How many miles do you drive to work?"); // miles drove daily
+var daysWorked = prompt("How many days do you work a week?"); //days worked a week
+var weeklyTotal = dailyMiles * daysWorked; // total miles drove in week.
 alert("You drive " +weeklyTotal+ " miles in a week! ")
