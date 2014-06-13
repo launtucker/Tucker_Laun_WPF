ccecc479692955e650a90cd4b00887aac5adfc8f
launtucker <launtucker@launs-MacBook-Pro.local>
2014-06-12 21:19:34 -0400

    added side notes
    added side notes

--- Expressions_Industry/JS/script.js
+++ Expressions_Industry/JS/script.js
@@ -2,9 +2,9 @@
  * Created by laun tucker on 6/12/14 Expression Industry.
  */
 
-var timelineHours = 4;
-var designHours = 36;
-var frameworkHours = 3;
-var totalHours = timelineHours + designHours + frameworkHours;
-var averageHours = totalHours / 7;
+var timelineHours = 4; // hours worked on the time line
+var designHours = 36; // hours worked on the whole design
+var frameworkHours = 3; //hours worked on the Framework.
+var totalHours = timelineHours + designHours + frameworkHours; // Hours it took to finish job
+var averageHours = totalHours / 7; //the average time spent on the whole project daily.
 console.log("I designed a website in the total of " +totalHours+ " in 7 days, \n averaging out to be " +averageHours+ " daily.");
