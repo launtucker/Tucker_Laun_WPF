4b2119b03322e8ed38acd7eda80e9d11ac6b5674
launtucker <launtucker@launs-MacBook-Pro.local>
2014-06-19 16:29:40 -0400

    Cond Industry
    finished code on user age ver.

--- Conditionals_Industry/JS/script.js
+++ Conditionals_Industry/JS/script.js
@@ -1,3 +1,20 @@
 /**
- * Created by launtucker on 6/19/14.
+ * Created by laun tucker on 6/19/14 Conditionals_Industry.
  */
+// validate if user is old enough to buy e-juice
+
+var userInput = prompt("Please type name?");
+var age = prompt("Please type your age?");
+
+// if the person is under the age of 18 they cant buy ejuice from the site.
+// if the are of age then they can purchase e-juice from the web site.
+
+if(age > 18){
+    //code that says they can buy products
+    console.log("Congratulations you can buy from this site")
+
+}else{
+    // if user is under age code prompts
+    console.log("Nice try kiddo..Come back when you are 18!")
+
+}
