434e7d63e5b3609969ef7756fea487d0a9d053fe
launtucker <launtucker@launs-MacBook-Pro.local>
2014-06-19 21:35:20 -0400

    movie ticket price
    finished

---
+++ Conditionals_worksheet/Movie Ticket/JS/script.js
@@ -0,0 +1,15 @@
+/**
+ * Created by laun tucker on 6/19/14 Movie Ticket Price.
+ */
+
+//The local movie theater in town has a ticket price of $12.00 but if you are a senior (55 and older),
+// under 10 you get the discounted price of $7.00. In addition, if a customer is seeing a movie between
+// 3pm and 5pm they can also get the discounted price.
+//Determine which of the two prices the customer is eligible for.
+
+var discountAge =55 > 100;
+var timeMovie = 3;
+var age = 56;
+var seniorPay = 12-7;
+
+console.log("Your Ticket price is " + seniorPay);
