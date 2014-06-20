3e643f66bb776a77bd616e071c4baec25596c7dd
launtucker <launtucker@launs-MacBook-Pro.local>
2014-06-19 21:59:22 -0400

    Stuff your face
    Finished

---
+++ Conditionals_worksheet/Degrees/JS/script.js
@@ -0,0 +1,5 @@
+/**
+ * Created by laun tucker on 6/19/14 Degrees.
+ */
+
+var degrees =
---
+++ Conditionals_worksheet/Stuff Your Face/JS/script.js
@@ -0,0 +1,20 @@
+/**
+ * Created by laun tucker on 6/19/14 Stuff Your face.
+ */
+
+// to determine weight for pie eating contest.
+
+var userInput = prompt("Please type weight in pounds");
+
+
+// if the person is under the under 250 lbs prompt.
+// if the person is over the under 250 lbs prompt.
+if(userInput > 250){
+    //code that says they can buy products
+    console.log("You qualifies for the heavyweight division!")
+
+}else{
+    // if user is under weight code prompts
+    console.log("Sorry you needs to gain some weight!")
+
+}
