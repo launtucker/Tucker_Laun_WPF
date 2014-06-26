a1ff0b50c2f1cd3e095181f08d675d6b240192f7
launtucker <launtucker@launs-MacBook-Pro.local>
2014-06-26 18:43:31 -0400

    added while to industry and started wacky
    dded while to industry and started wacky

--- Functions/Personal/js/script.js
+++ Functions/Personal/js/script.js
@@ -1,3 +1,8 @@
 /**
  * Created by laun tucker on 6/26/14 Personal Function.
  */
+    //Creating a ramdon amount of miles you want to walk in a month
+
+var minMiles = prompt("The min amount of miles you want to walk in a day?");
+var maxMiles = prompt("The max amount of miles you want to walk in a day?");
+
--- Functions/Wacky/js/script.js
+++ Functions/Wacky/js/script.js
@@ -1,3 +1,18 @@
  /**
  * Created by laun tucker on 6/26/14 Wacky Function.
  */
+//creating a random set of numbers for football teams jerseys
+
+ var lowNumber = prompt("Give the lowest number for Jerseys!");
+
+ while (isNaN(lowNumber) || lowNumber===""){
+     //reprompt user
+    lowNumber = prompt("PLEASE type a LOW number!")
+ }
+
+ var highestNumber = prompt("Give the highest number you want to go?");
+
+ while (isNaN(highestNumber) || highestNumberr==="") {
+     //reprompt user
+     highestNumber = prompt("PLEASE type a HIGHTEST number!")
+ }
