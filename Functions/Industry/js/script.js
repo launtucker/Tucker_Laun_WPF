926b069e2392e5674e1575b55620af0d22adee3c
launtucker <launtucker@launs-MacBook-Pro.local>
2014-06-26 18:03:18 -0400

    Industry code
    if and else code

--- Functions/Industry/js/script.js
+++ Functions/Industry/js/script.js
@@ -1,3 +1,16 @@
 /**
  * Created by laun tucker on 6/26/14  Industry Function.
  */
+
+// Not to use picture over 800 pixels
+
+var userInput = prompt("How many pixels is the picture you want to use?");
+var maxPixels = 800;
+
+if (userInput <800){
+    console.log("You are able to use that picture!")
+}else{
+    console.log("Please find a smaller picture!")
+
+
+}
