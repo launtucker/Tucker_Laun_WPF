0af8104bf7afb5e604d767bf82d6f07ff677b7f8
launtucker <launtucker@launs-MacBook-Pro.local>
2014-06-26 18:57:05 -0400

    fixed errors in markup
    fixed errors in markup

--- Functions/Personal/js/script.js
+++ Functions/Personal/js/script.js
@@ -4,5 +4,14 @@
     //Creating a ramdon amount of miles you want to walk in a month
 
 var minMiles = prompt("The min amount of miles you want to walk in a day?");
+
+while (isNaN(minMiles) || minMiles==="") {
+    //reprompt user
+    minMiles = prompt("PLEASE type a minimum NUMBER of miles!")
+}
 var maxMiles = prompt("The max amount of miles you want to walk in a day?");
 
+while (isNaN(maxMiles) || maxMiles==="") {
+    //reprompt user
+    maxMiles = prompt("PLEASE type a maximum NUMBER of miles!")
+}
--- Functions/Wacky/js/script.js
+++ Functions/Wacky/js/script.js
@@ -2,17 +2,20 @@
  * Created by laun tucker on 6/26/14 Wacky Function.
  */
 //creating a random set of numbers for football teams jerseys
-
+// give the starting number for Jerseys
  var lowNumber = prompt("Give the lowest number for Jerseys!");
-
+//prompt user for number only
  while (isNaN(lowNumber) || lowNumber===""){
      //reprompt user
     lowNumber = prompt("PLEASE type a LOW number!")
  }
-
+//Ask user for the highest number for jerseys.
  var highestNumber = prompt("Give the highest number you want to go?");
 
- while (isNaN(highestNumber) || highestNumberr==="") {
+ //prompt user for number only
+ while (isNaN(highestNumber) || highestNumber==="") {
      //reprompt user
      highestNumber = prompt("PLEASE type a HIGHTEST number!")
  }
+
+
