a79a5ebfd6c221462d61ecc40ab17405702a6f33
launtucker <launtucker@launs-MacBook-Pro.local>
2014-06-26 22:47:35 -0400

    did circ. of circle
    did circ. of circle

---
+++ Functions_worksheet/Circumference/JS/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by laun tucker on 6/26/14 Circumference.
+ */
+
+//Calculate the Area of a circle
+var diam = 23;
+var radius = 10;
+var area = calculateArea(diam, radius);
+console.log("The area of the circle " + area);
+
+function calculateArea(d, r )
+{
+    return d *r;
+}
