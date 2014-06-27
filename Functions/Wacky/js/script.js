76be2d32474c6c1b0c2d9e343a47ef09a382f8ae
launtucker <launtucker@launs-MacBook-Pro.local>
2014-06-26 21:40:13 -0400

    Cleaned notes up in wacky
    Cleaned notes up in wacky

--- Functions/Wacky/js/script.js
+++ Functions/Wacky/js/script.js
@@ -2,19 +2,24 @@
  * Created by laun tucker on 6/26/14 Wacky Function.
  */
 //creating a random set of numbers for football teams jerseys
+
 // give the starting number for Jerseys
  var lowNumber = prompt("Give the lowest number for Jerseys!");
+
 //prompt user for number only
  while (isNaN(lowNumber) || lowNumber===""){
-     //reprompt user
+
+     //reprompt user for just numbers
     lowNumber = prompt("PLEASE type a LOW number!")
  }
+
 //Ask user for the highest number for jerseys.
  var highestNumber = prompt("Give the highest number you want to go?");
 
  //prompt user for number only
  while (isNaN(highestNumber) || highestNumber==="") {
-     //reprompt user
+
+     //reprompt user only nmbers
      highestNumber = prompt("PLEASE type a HIGHTEST number!")
  }
 //catching results
