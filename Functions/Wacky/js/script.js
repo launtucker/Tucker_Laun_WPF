f3777cf72c34ced641433b09174f28d711b04f0c
launtucker <launtucker@launs-MacBook-Pro.local>
2014-06-26 20:10:49 -0400

    marked up code in Wacky
    marked up code in Wacky

--- Functions/Wacky/js/script.js
+++ Functions/Wacky/js/script.js
@@ -17,15 +17,17 @@
      //reprompt user
      highestNumber = prompt("PLEASE type a HIGHTEST number!")
  }
-
+//catching results 
  var results = jerseyNumber(lowNumber, highestNumber);
 console.log(results);
-
+//creating / generating list of random numbers
  function jerseyNumber (minNum, maxNum){
 
      var numberList = Math.round(Math.random( ) * (maxNum-minNum) + Number(minNum));
      return numberList;
  }
+
+ //creating 30 jersey number to send off to printers.
 for (var i=0; i<30; i++){
     console.log(jerseyNumber(lowNumber, highestNumber) + " Random Jersey Numbers");
 
